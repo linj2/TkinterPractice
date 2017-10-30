1824a7c4039551b2886525d3cbf7a6d83589b149
Jing Lin <linj2@rose-hulman.edu>
2017-10-30 11:57:25 -0400

    done

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -2,8 +2,8 @@
 This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jing Lin.
+"""  # TODONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -12,26 +12,28 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # ------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # ------------------------------------------------------------------
-
+    root = tkinter.Tk()
     # ------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # ------------------------------------------------------------------
-
+    frame1 = ttk.Frame(root, padding=100)
+    frame1.grid()
     # ------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # ------------------------------------------------------------------
-
+    print_hello_button = ttk.Button(frame1, text='Print Hello')
+    print_hello_button.grid()
     # ------------------------------------------------------------------
     # TODO: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # ------------------------------------------------------------------
-
+    print_hello_button['command'] = lambda: print('Hello')
     # ------------------------------------------------------------------
     # TODO: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
@@ -40,7 +42,12 @@ def main():
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # ------------------------------------------------------------------
+    an_entry_box = ttk.Entry(frame1)
+    an_entry_box.grid()
 
+    print_entry_button = ttk.Button(frame1, text='Print entry')
+    print_entry_button['command'] = lambda: print_contents(an_entry_box)
+    print_entry_button.grid()
     # ------------------------------------------------------------------
     # TODO: 7.
     #    -- Put a second Entry on the Frame.
@@ -64,11 +71,31 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
+    second_entry_box = ttk.Entry(frame1)
+    second_entry_box.grid()
 
+    print_N_button = ttk.Button(frame1, text='Print N times from first entry')
+    print_N_button['command'] = lambda: print_N_times(an_entry_box,second_entry_box)
+    print_N_button.grid()
     # ------------------------------------------------------------------
     # TODO: 8. As time permits, do other interesting GUI things!
     # ------------------------------------------------------------------
 
+    root.mainloop()
+
+
+def print_contents(entry_box):
+    contents_of_entry_box = entry_box.get()
+    if contents_of_entry_box == 'ok':
+        print('Hello')
+    else:
+        print('Goodbye')
+
+def print_N_times(first_entry, second_entry):
+    contents = first_entry.get()
+    n = int(second_entry.get())
+    for k in range(n):
+        print(contents)
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
